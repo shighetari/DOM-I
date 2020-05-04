e3a6ececf04e6985451b7f58d35ce0b6e92e3f7e
Francisco Barrios <shighetari@gmail.com>
2020-05-04 17:47:46 -0400

    final commit

--- js/index.js
+++ js/index.js
@@ -40,3 +40,125 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// start of my work
+
+// start of nav links 
+
+//services
+const firstLink = document.querySelector('nav a:nth-child(1)')
+firstLink.textContent = siteContent.nav['nav-item-1'];
+//product
+const secondLink = document.querySelector('nav a:nth-child(2)')
+secondLink.textContent = siteContent.nav['nav-item-2'];
+//vision
+const thirdLink = document.querySelector('nav a:nth-child(3)')
+thirdLink.textContent = siteContent.nav['nav-item-3'];
+//features
+const fourthLink = document.querySelector('nav a:nth-child(4)')
+fourthLink.textContent = siteContent.nav['nav-item-4'];
+//about
+const fithLink = document.querySelector('nav a:nth-child(5)')
+fithLink.textContent = siteContent.nav['nav-item-5'];
+//contact
+const sixthLink = document.querySelector('nav a:nth-child(6)')
+sixthLink.textContent = siteContent.nav['nav-item-6'];
+//end of nav links
+
+//start of cta (dom is awesome)
+const CTAh1 = document.querySelector('h1');
+CTAh1.textContent = siteContent['cta']['h1'];
+// end of dom is awesome
+
+//start of button 
+const button1 = document.querySelector('button');
+button1.textContent = siteContent['cta']['button'];
+//end of button
+
+//start of img
+const img1 = document.getElementById('cta-img');
+img1.setAttribute('src', siteContent['cta']['img-src']) ;
+// end of logo img
+
+//start of body stuff~~~~
+
+//features
+const features = document.querySelector('h4');
+features.textContent = siteContent['main-content']['features-h4'];
+// end of features
+
+//start of feature body h4
+const featuresbody = document.querySelector('p');
+featuresbody.textContent = siteContent['main-content']['features-content'];
+//end
+
+//start of h4s
+const allh4s = document.querySelectorAll('h4');
+allh4s[1].textContent = siteContent['main-content']['about-h4'];
+allh4s[2].textContent = siteContent['main-content']['services-h4'];
+allh4s[3].textContent = siteContent['main-content']['product-h4'];
+allh4s[4].textContent = siteContent['main-content']['vision-h4'];
+// end of h4s
+//start of content
+const allps = document.querySelectorAll('p');
+allps[1].textContent = siteContent['main-content']['about-content'];
+allps[2].textContent = siteContent['main-content']['services-content'];
+allps[3].textContent = siteContent['main-content']['product-content'];
+allps[4].textContent = siteContent['main-content']['vision-content'];
+// end of content
+//start of middle img
+const middleimage = document.getElementById('middle-img');
+middleimage.setAttribute('src', siteContent['main-content']['middle-img-src']) ;
+// end of middle img
+
+//start of contact (footer)
+
+//start of contact
+const contactfooter = document.querySelector('.contact h4');
+contactfooter.textContent = siteContent['contact']['contact-h4'];
+// end of contact h4
+allps[5].textContent = siteContent['contact']['address'];
+allps[6].textContent = siteContent['contact']['phone'];
+allps[7].textContent = siteContent['contact']['email'];
+//end contact footer
+
+//start of copy right
+const copyrightfooter = document.querySelector('footer');
+copyrightfooter.textContent = siteContent['footer']['copyright'];
+//end of copy right
+
+// task 3
+
+//change text to green
+let navLinks = document.querySelectorAll(`nav a`);
+for(element of navLinks){element.style.color = "green"};
+//end of green text nav links
+
+//start make new links
+
+
+const newLink = document.createElement('a')
+
+ newLink.href = '#';
+ 
+newLink.textContent = 'ZZZZZZ'
+// newLink.classList.add(newLinkProps.className)
+const nav = document.querySelector('nav');
+nav.appendChild(newLink)
+
+const newLink1 = document.createElement('a')
+newLink1.textContent = 'Zzzz2?'
+nav.prepend(newLink1);
+newLink1.href = '#';
+
+
+
+
+
+//start of trash
+// var nav = newLink.appendChild(newChild);
+
+// var nav = document.createElement("a");                
+// var newLink = document.createTextNode("Water");        
+// nav.appendChild(newLink);                              
+
